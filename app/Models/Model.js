dbf9b30a7326b0348cc5c87d6926bd8095f75a83
Nitish Kumar <mintu.nitish@gmail.com>
2018-11-02 17:38:45 +0530

    Model Init
    Signed-off-by: Nitish Kumar <nitish.kumar@sugaldamani.com>

---
+++ app/Models/Model.js
@@ -0,0 +1,16 @@
+/*
+ * This file is part of the awesome-people/mirandum package.
+ *
+ * Copyright (c) 2018, Nitish Kumar <mintu.nitish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/awesome-people/mirandum
+ * 
+ * Created By: Nitish Kumar on 2/11/18 5:37 PM
+ */
+
+class Model {
+    constructor() {}
+}
